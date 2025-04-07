dd7a6e36419c51fcbc3edc6e9a8676731662189e
OopsNidhiCodes <nidhipm2005@gmail.com>
2025-04-07 14:21:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const sections = document.querySelectorAll('.section');
+
+const revealSections = () => {
+  const triggerBottom = window.innerHeight * 0.85;
+
+  sections.forEach(section => {
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < triggerBottom) {
+      section.classList.add('show');
+    }
+  });
+};
+
+window.addEventListener('scroll', revealSections);
+window.addEventListener('load', revealSections);
